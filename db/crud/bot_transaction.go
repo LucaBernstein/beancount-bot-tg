c4fb78b67645de64b04252dcbe0d53626f2c7b91
Luca Bernstein <luca@lucabernstein.com>
2021-10-28 17:30:53 +0200

    Fix typo

--- db/crud/bot_transaction.go
+++ db/crud/bot_transaction.go
@@ -1,7 +1,7 @@
 package crud
 
 func (r *Repo) RecordTransaction(chatId int64, tx string) error {
-	_, err := r.db.Exec(`INSERT INTO "bot::transactions" ("tgChatId", "value")
+	_, err := r.db.Exec(`INSERT INTO "bot::transaction" ("tgChatId", "value")
 		VALUES ($1, $2);`, chatId, tx)
 	return err
 }
