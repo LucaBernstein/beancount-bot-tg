e3a9f931891cfccf28a47b5e9c9f6f3d9fef9ed5
Luca Bernstein <luca@lucabernstein.com>
2021-12-27 09:14:39 +0100

    Write additional tests

---
+++ helpers/arrays_test.go
@@ -0,0 +1,27 @@
+package helpers_test
+
+import (
+	"testing"
+
+	"github.com/LucaBernstein/beancount-bot-tg/helpers"
+)
+
+func TestArrayMatching(t *testing.T) {
+	if helpers.ArrayContains([]string{"a", "b", "c"}, "d") {
+		t.Error("'d' should not be found in array")
+	}
+
+	if !helpers.ArrayContains([]string{"a", "b", "c"}, "c") {
+		t.Error("'c' should be found in array")
+	}
+
+	if !helpers.ArrayContainsC([]string{"a", "b", "big"}, "BIG", false) {
+		t.Error("'BIG' (ci) should be found in array")
+	}
+
+	if helpers.ArraysEqual([]string{"a"}, []string{"b"}) {
+		t.Error("ArraysEqual should fail for different arrays")
+	}
+
+	// TODO: ArraysEqual 83.3%
+}
