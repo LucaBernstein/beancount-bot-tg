1db1c862602e3043ca99988c5f5ce421cdb204d3
Luca Bernstein <luca@lucabernstein.com>
2022-11-14 17:23:38 +0100

    Fix user settings available for omitLeadingSlash

---
+++ db/migrations/v12.go
@@ -0,0 +1,21 @@
+package migrations
+
+import (
+	"database/sql"
+	"log"
+)
+
+func v12(db *sql.Tx) {
+	v12AddLeadingSlashSetting(db)
+}
+
+func v12AddLeadingSlashSetting(db *sql.Tx) {
+	sqlStatement := `
+	INSERT INTO "bot::userSettingTypes" ("setting", "description") VALUES
+		('user.omitCommandSlash', 'make leading slash for commands optional');
+	`
+	_, err := db.Exec(sqlStatement)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
